ae274f93cc3bf876eb913b32f0189eae062e9a98
Vanessa Toro <vanetoro84@gmail.com>
2018-02-27 19:48:31 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
-//define functions here
+
+function getIt(){
+$('p').click(function(){
+    alert('Hey!');
+});
+}
+
+function frameIt(){
+  $('img').addClass('tasty');
+
+}
+
+function pressIt(){
+  $('document').on('keydown',function(){
+   if (event.which == 71){
+     alert('g has been pressed')
+   }
+ });
+}
+
+function submitIt(){
+  $('input:submit').on('click', function(){
+    alert('Your form is going to be submitted now.')
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+frameIt();
+pressIt();
+getIt();
+submitIt();
 });
